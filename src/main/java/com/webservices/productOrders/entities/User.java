6a1c0212ec9249f62d32ea52f6d4ce0af9238f80
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2022-11-19 14:38:14 -0300

    Configuring JPA and H2 database and add JPA mapping in entities

--- src/main/java/com/webservices/productOrders/entities/User.java
+++ src/main/java/com/webservices/productOrders/entities/User.java
@@ -1,13 +1,23 @@
 package com.webservices.productOrders.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;

6fc657b320e3474cc51e985910a893a16ec8acf1
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2022-11-21 14:48:04 -0300

    Added service layer for users and added new endpoint for findById

--- src/main/java/com/webservices/productOrders/resources/UserResource.java
+++ src/main/java/com/webservices/productOrders/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.webservices.productOrders.resources;
 
 import com.webservices.productOrders.entities.User;
+import com.webservices.productOrders.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "User Test", "test@gmail.com", "999999999", "test");
-    return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User u = userService.findById(id);
+        return ResponseEntity.ok().body(u);
     }
 }
